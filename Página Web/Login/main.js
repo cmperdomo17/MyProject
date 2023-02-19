cd12ed8563891b0572ac05d454e2dd89f5dfe054
Carlos Mario Perdomo Ramos <122324408+cmperdomo17@users.noreply.github.com>
2023-02-19 11:38:58 -0500

    jeje

--- Página Web/Login/main.js
+++ Página Web/Login/main.js
@@ -6,9 +6,10 @@ login.addEventListener('click', (e) => {
     e.preventDefault()
     const data = {
         usuario: usuario.value,
-        contraseña: contraseña.value
+        contraseña: contraseña.value      
     }
     console.log(data)
+
 });
 
 const btnSwitch = document.querySelector('#switch');
@@ -17,5 +18,19 @@ btnSwitch.addEventListener('click', () =>{
     btnSwitch.classList.toggle('active');
 });
 
+function inicio_sesion(){
+    var user, password
+    user=document.getElementById("usuario").value;
+    password=document.getElementById("contraseña").value;
 
-
+    if (user == "mario" && password == "1234"){
+        alert("Iniciaste Sesión")
+    } else if(user == "" && password == ""){
+        alert("Datos incompletos")
+    } else{
+        alert("Datos incorrectos")
+    }
+}
+document.getElementById("login").onclick = function() {
+    inicio_sesion();
+}
